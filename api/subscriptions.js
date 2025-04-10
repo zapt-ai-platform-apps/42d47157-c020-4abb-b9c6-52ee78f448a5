c8e27277eba6ac868db11ae064cf21d9b4ddb3eb
David Fox <davidmfox123@gmail.com>
2025-04-10 18:51:20 +0100

    Update subscriptions.js

--- api/subscriptions.js
+++ api/subscriptions.js
@@ -107,6 +107,9 @@ export default async function handler(req, res) {
           subscription_data: {
             application_fee_percent: 30
           }
+        },
+        {
+          stripe_account: 'acct_1Myei9AwvseTMg9w'
         });
         
         console.log(`Created checkout session: ${session.id}, URL: ${session.url}`);
